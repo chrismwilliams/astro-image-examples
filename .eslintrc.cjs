36a4b323bceb4188f9f181d3e61092f08070597a
Chris Williams <chrismwilliams@users.noreply.github.com>
2024-05-02 09:02:39 +0100

    remove @unocss plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,31 +1,30 @@
 /** @type {import("@types/eslint").Linter.Config} */
 module.exports = {
-	ignorePatterns: ["node_modules", "dist"],
-	root: true,
-	env: {
-		node: true,
-	},
-	parser: "@typescript-eslint/parser",
-	plugins: ["@typescript-eslint", "prettier"],
-	extends: [
-		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended",
-		"plugin:prettier/recommended",
-		"plugin:astro/recommended",
-		"@unocss",
-	],
-	overrides: [
-		{
-			files: ["*.astro"],
-			parser: "astro-eslint-parser",
-			parserOptions: {
-				parser: "@typescript-eslint/parser",
-				extraFileExtensions: [".astro"],
-			},
-			rules: {
-				"prettier/prettier": "off",
-				"@typescript-eslint/no-unused-vars": ["warn", { varsIgnorePattern: "Props" }],
-			},
-		},
-	],
+  ignorePatterns: ["node_modules", "dist"],
+  root: true,
+  env: {
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "plugin:astro/recommended",
+  ],
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+      rules: {
+        "prettier/prettier": "off",
+        "@typescript-eslint/no-unused-vars": ["warn", { varsIgnorePattern: "Props" }],
+      },
+    },
+  ],
 };
